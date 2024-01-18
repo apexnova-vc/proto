ab309b1fdecf648f2eb6ddaf72f87968c711b986
Dongming Shen <dongmingshen@icloud.com>
2024-01-18 12:52:45 +0800

    OKK

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,20 +5,39 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ktlint) apply false
+    `java-library`
+    `maven-publish`
 }
 
 group = "com.apexnova"
 
-subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        filter {
-            exclude {
-                it.file.path.startsWith(project.layout.buildDirectory.get().dir("generated").toString())
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/apexnova-vc/proto")
+            credentials {
+                username = "dongming-shen"
+                password = "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"
             }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+                groupId = "com.apexnova"
+                artifactId = "protos"
+                version = "0.0.1"
         }
     }
 }
 
-tasks.create("assemble").dependsOn(":server:installDist")
+// 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
+
+// todo: maybe use variants / configurations to do both stub & stub-lite here
+
+// Note: We use the java-library plugin to get the protos into the artifact for this subproject
+// because there doesn't seem to be an better way.
+
+java {
+    sourceSets.getByName("main").resources.srcDir("src/main/proto")
+}
